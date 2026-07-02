2bbc52794f452407cdb101c3d059e7b99372fd6d
JoeAbraham27 <joeabraham2772@gmail.com>
2026-07-02 22:24:54 +0530

    Add files via upload

---
+++ day 1/First.js/script.js
@@ -0,0 +1,27 @@
+console.log("welcome")
+document.write("hello cek")
+//single line comment
+/*
+multiline
+*/
+let name1="Joe Abraham"
+console.log(name1)
+console.log(typeof(name1)) //return datatype of variable
+let salary=50000
+console.log(salary)
+console.log(typeof(salary))
+var invalid=false
+console.log(invalid)
+console.log(typeof(invalid))
+const pi=3.14
+console.log(pi)
+let MyAge
+console.log(MyAge)
+sum=1+2
+console.log(sum)
+difference=3-4
+console.log(difference)
+product=1*2
+console.log(product)
+Quotient=4/2
+console.log(Quotient)
