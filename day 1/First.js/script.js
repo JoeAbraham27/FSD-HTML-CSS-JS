6b2cd8211c575d333e50208176802c3899e73d2b
JoeAbraham27 <joeabraham2772@gmail.com>
2026-07-02 23:04:46 +0530

    Refactor script.js with updated variable names and logic

--- day 1/First.js/script.js
+++ day 1/First.js/script.js
@@ -1,27 +1,35 @@
-console.log("welcome")
-document.write("hello cek")
-//single line comment
-/*
-multiline
-*/
-let name1="Joe Abraham"
+console.log("Hello World")
+document.write("Hello Graca")
+
+let name1 = "Johan"
 console.log(name1)
-console.log(typeof(name1)) //return datatype of variable
-let salary=50000
-console.log(salary)
-console.log(typeof(salary))
-var invalid=false
+console.log(typeof (name1))
+
+let salary = 80000
+console.log(typeof (salary))
+var invalid = false
 console.log(invalid)
-console.log(typeof(invalid))
-const pi=3.14
+console.log(typeof (invalid))
+const pi = 3.14
 console.log(pi)
-let MyAge
-console.log(MyAge)
-sum=1+2
-console.log(sum)
-difference=3-4
-console.log(difference)
-product=1*2
-console.log(product)
-Quotient=4/2
-console.log(Quotient)
+
+let myAge
+console.log(myAge)
+
+let num1 = 10
+let num2 = 5
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+console.log(num1 ** num2)
+
+let x = 5
+x += 5
+console.log(x)
+
+let flag1 = true
+let flag2 = false
+console.log(flag1 == flag2)
+console.log(flag1 !== flag2)
